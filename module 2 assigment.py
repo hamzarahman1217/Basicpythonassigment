6c905d7ae9945c807e95fe3b04bed6a2ad2d4e8d
hamzarahman1217 <rahmanhamza1217@gmail.com>
2025-07-28 11:14:39 +0530

    Add myfile.txt

---
+++ module 2 assigment.py
@@ -0,0 +1,20 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+full_name = first_name + " " + last_name
+print(f"\n Hello  {full_name} welcome to the python programm !")
